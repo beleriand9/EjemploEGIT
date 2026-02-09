45a47c2330b75fa461e7442f5beb81e98d767f71
Ana Belen Rueda <anabelenruerei@gmail.com>
2026-02-09 09:03:45 +0100

    Cambio rama 1

--- src/EjemploEGIT/EjemploEGIT.java
+++ src/EjemploEGIT/EjemploEGIT.java
@@ -1,12 +1,19 @@
 package EjemploEGIT;
 
+import java.util.Scanner;
+
 public class EjemploEGIT {
 
 	public static void main(String[] args) {
 
+		Scanner teclado = new Scanner (System.in);
+		
 		System.out.println("Ejemplo GIT desde Eclipse");
 		System.out.println("Primer cambio desde master");
-	
+		System.out.println("Primer cambio Rama 1");
+		
+		String prueba = teclado.nextLine();
+		
 	}
 
 }
